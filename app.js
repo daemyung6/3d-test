c25bbe972c82bde1d1cf68bfccb6fda73140d548
Hong Dae Myung <daemyung6@gmail.com>
2022-05-26 19:17:26 +0900

    이동 추가

--- app.js
+++ app.js
@@ -1,15 +1,109 @@
 import * as THREE from './build/three.module.js';
-
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
 import Character from './character.js';
 
-let camera, scene, renderer, object;
+export let scene;
+export let physicsWorld;
+let camera, renderer, object;
 let controls;
 let mixer;
+const margin = 0.01;
 
-const displayDiv = document.createElement('div');;
+let transformAux1;
+let tempBtVec3_1;
+let rigidBodies = [];
+
+const displayDiv = document.createElement('div');
 displayDiv.classList.add('displayDiv');
 document.body.appendChild(displayDiv);
+
+function createParalellepipedWithPhysics(sx, sy, sz, mass, pos, quat, material) {
+
+  const object = new THREE.Mesh(new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1), material);
+  const shape = new Ammo.btBoxShape(new Ammo.btVector3(sx * 0.5, sy * 0.5, sz * 0.5));
+  shape.setMargin(margin);
+
+  createRigidBody(object, shape, mass, pos, quat);
+
+  return object;
+
+}
+
+function createRigidBody(object, physicsShape, mass, pos, quat, vel, angVel) {
+
+  if (pos) {
+
+      object.position.copy(pos);
+
+  } else {
+
+      pos = object.position;
+
+  }
+
+  if (quat) {
+
+      object.quaternion.copy(quat);
+
+  } else {
+
+      quat = object.quaternion;
+
+  }
+
+  const transform = new Ammo.btTransform();
+  transform.setIdentity();
+  transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+  transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+  const motionState = new Ammo.btDefaultMotionState(transform);
+
+  const localInertia = new Ammo.btVector3(0, 0, 0);
+  physicsShape.calculateLocalInertia(mass, localInertia);
+
+  const rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, physicsShape, localInertia);
+  const body = new Ammo.btRigidBody(rbInfo);
+
+  body.setFriction(0.5);
+
+  if (vel) {
+
+      body.setLinearVelocity(new Ammo.btVector3(vel.x, vel.y, vel.z));
+
+  }
+
+  if (angVel) {
+
+      body.setAngularVelocity(new Ammo.btVector3(angVel.x, angVel.y, angVel.z));
+
+  }
+
+  object.userData.physicsBody = body;
+  object.userData.collided = false;
+
+  if (mass > 0) {
+
+      rigidBodies.push(object);
+
+      // Disable deactivation
+      body.setActivationState(4);
+
+  }
+
+  physicsWorld.addRigidBody(body);
+
+  return body;
+
+}
+
+
+export let Ammo;
+
+AmmoInit().then(function (m) {
+  console.log(m);
+  Ammo = m;
+  
+  init();
+})
 
 
 function init() {
@@ -55,16 +149,42 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
   });
 
-  const geometry = new THREE.BoxGeometry( 1000, 1, 1000 );
-  const material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  // const geometry = new THREE.BoxGeometry( 1000, 1, 1000 );
+  
+  // const cube = new THREE.Mesh( geometry, material );
+  // scene.add( cube );
+
+
+
+  // Physics configuration
+  // ---------------------
+  let m_collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+  let m_dispatcher = new Ammo.btCollisionDispatcher(m_collisionConfiguration);
+  let m_broadphase = new Ammo.btDbvtBroadphase();
+  let m_constraintSolver = new Ammo.btSequentialImpulseConstraintSolver();
+
+  physicsWorld = new Ammo.btDiscreteDynamicsWorld(m_dispatcher, m_broadphase, m_constraintSolver, m_collisionConfiguration);
+  physicsWorld.setGravity(new Ammo.btVector3(0, -9.810, 0));
+
+  Ammo.btGImpactCollisionAlgorithm.prototype.registerAlgorithm(physicsWorld.getDispatcher());
+
+  transformAux1 = new Ammo.btTransform();
+  tempBtVec3_1 = new Ammo.btVector3(0, 0, 0);
+
+
+  // Ground
+  let pos = new THREE.Vector3();
+  let quat = new THREE.Quaternion();
+  pos.set(0, - 0.5, 0);
+  quat.set(0, 0, 0, 1);
+  const ground = createParalellepipedWithPhysics(1000, 1, 1000, 0, pos, quat, new THREE.MeshPhongMaterial({ color: 0xFFFFFF }));
+  scene.add(ground);
+  ground.receiveShadow = true;
+  ground.material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+
 
   const character = new Character();
-  character.onload = function() {
-    scene.add(character.FBX.standing);
-    renderList.push(character.update);
-  }
+  character.onload = function() { console.log('load character') }
 
   render();
 }
@@ -75,14 +195,40 @@ function init() {
 
 
 export let renderList = [];
+const clock = new THREE.Clock();
+let deltaTime = clock.getDelta();
 function render() {
   requestAnimationFrame( render );
+  deltaTime = clock.getDelta();
 
   for (let i = 0; i < renderList.length; i++) {
     renderList[i]();
   }
 
+  // Step world
+  physicsWorld.stepSimulation(deltaTime, 10);
+
+  // Update rigid bodies
+  for (let i = 0, il = rigidBodies.length; i < il; i++) {
+
+      const objThree = rigidBodies[i];
+      const objPhys = objThree.userData.physicsBody;
+      const ms = objPhys.getMotionState();
+
+      if (ms) {
+
+          ms.getWorldTransform(transformAux1);
+          const p = transformAux1.getOrigin();
+          const q = transformAux1.getRotation();
+          objThree.position.set(p.x(), p.y(), p.z());
+          objThree.quaternion.set(q.x(), q.y(), q.z(), q.w());
+
+          objThree.userData.collided = false;
+
+      }
+
+  }
+
   renderer.render(scene, camera);
 }
 
-init();
--- character.js
+++ character.js
@@ -1,5 +1,58 @@
 import * as THREE from './build/three.module.js';
 import { FBXLoader } from './jsm/loaders/FBXLoader.js';
+import * as app from './app.js';
+
+
+function createBox(pos, quat, w, l, h, mass, friction) {
+    var material = mass > 0 ? materialDynamic : materialStatic;
+    var shape = new THREE.BoxGeometry(w, l, h, 1, 1, 1);
+    var geometry = new Ammo.btBoxShape(new Ammo.btVector3(w * 0.5, l * 0.5, h * 0.5));
+
+    if(!mass) mass = 0;
+    if(!friction) friction = 1;
+
+    var mesh = new THREE.Mesh(shape, material);
+    mesh.position.copy(pos);
+    mesh.quaternion.copy(quat);
+    scene.add( mesh );
+
+    var transform = new Ammo.btTransform();
+    transform.setIdentity();
+    transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+    transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+    var motionState = new Ammo.btDefaultMotionState(transform);
+
+    var localInertia = new Ammo.btVector3(0, 0, 0);
+    geometry.calculateLocalInertia(mass, localInertia);
+
+    var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, geometry, localInertia);
+    var body = new Ammo.btRigidBody(rbInfo);
+
+    body.setFriction(friction);
+    // body.setRestitution(.9);
+    // body.setDamping(0.2, 0.2);
+
+    physicsWorld.addRigidBody( body );
+
+    if (mass > 0) {
+        body.setActivationState(DISABLE_DEACTIVATION);
+        // Sync physics and graphics
+        function sync(dt) {
+            var ms = body.getMotionState();
+            if (ms) {
+                ms.getWorldTransform(TRANSFORM_AUX);
+                var p = TRANSFORM_AUX.getOrigin();
+                var q = TRANSFORM_AUX.getRotation();
+                mesh.position.set(p.x(), p.y(), p.z());
+                mesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
+            }
+        }
+
+        syncList.push(sync);
+    }
+}
+
+
 
 export default function () {
     const that = this;
@@ -24,6 +77,8 @@ export default function () {
             if (FBXList.length === FBXloadCount) {
                 loadDone();
                 that.onload();
+
+                
             }
         });
     }
@@ -36,6 +91,7 @@ export default function () {
         console.log(that)
         const clock = new THREE.Clock();
         const mixer = new THREE.AnimationMixer( that.FBX.standing );
+
         const standing = mixer.clipAction( that.FBX.standing.animations[0] );
         anime.push(standing);
         standing.play()
@@ -62,20 +118,45 @@ export default function () {
             }
         })
 
+        let speed = 0;
+        const maxSpeed = 5; 
 
-        that.update = function() {
+        const quaternion = new THREE.Quaternion();
+        
+        window.test = that.FBX.standing;
+
+        const moveX = 100;
+        const moveZ = 100;
+
+        app.scene.add(that.FBX.standing);
+        app.renderList.push(function() {
+            if(keys.w) { 
+                speed += 0.5;
+                if(speed > maxSpeed ) { speed = maxSpeed; }
+            }
+            else {
+                speed -= 0.3;
+            }
+
+            if(speed < 0) { speed = 0; }
+            if(keys.s) {
+
+            }
+            if(keys.a) {
+                that.FBX.standing.rotation.y +=  0.1;
+                if(that.FBX.standing.rotation.y > Math.PI * 2) { that.FBX.standing.rotation.y = 0 }
+            }
+            if(keys.d) {
+                that.FBX.standing.rotation.y -=  0.1;
+                if(that.FBX.standing.rotation.y < 0) { that.FBX.standing.rotation.y = Math.PI * 2 }
+            }
+
+            that.FBX.standing.position.x += Math.sin(that.FBX.standing.rotation.y) * speed;
+            that.FBX.standing.position.z += Math.cos(that.FBX.standing.rotation.y) * speed;
+
             mixer.update( clock.getDelta() );
-            anime[0].weight = 1 - Number(input.value) / 100;
-            anime[1].weight = Number(input.value) / 100;
-        }
+            anime[0].weight = 1 - (speed / maxSpeed);
+            anime[1].weight = (speed / maxSpeed);
+        });
     }
-
-    const input = document.createElement('input');
-    input.style.cssText = `
-        position: absolute;
-        top: 0px;
-    `
-    input.setAttribute('type', 'range');
-    document.body.appendChild(input);
-
 }
